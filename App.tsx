fca75491dbfd46b6c18e9a2cac7152e4aa130d0f
Muhammad Kashani <muhammadkashani.outlook.com>
2024-01-09 09:02:51 -0600

    chnaged background to black Made changes to home screen

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "blue",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
   },
