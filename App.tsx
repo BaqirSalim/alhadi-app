cd3f1f2bbddb72f455ef1a930d614379363ecf15
Muhammad Kashani <muhammadkashani.outlook.com>
2024-01-18 09:05:42 -0600

    fixed issue5

--- App.tsx
+++ App.tsx
@@ -17,4 +17,12 @@ const styles = StyleSheet.create({
   },
 });
 
-const dummyAnouncments = ["free food on Wednesday", "wear festive clothes for 13th Rajab", "eat more food to prevent cancer", "sign up for SAT prep", "Protest for Palestine in downtown houston on Jan 26th", "sign up for highschool fieldtrip on Feb 16th"];
+const [dummyAnouncements, setDummyAnouncements] = useState ([
+  {anouncement: "free food on Wednesday", key: "1"},
+  {anouncement: "wear festive clothes for 13th Rajab", key: "2"},
+  {anouncement: "eat more food to prevent cancer", key: "3"},
+  {anouncement: "sign up for SAT prep", key: "4"},
+  {anouncement: "Protest for Palestine in downtown houston on Jan 26th", key: "5"},
+  {anouncement: "sign up for highschool fieldtrip on Feb 16th", key: "6"},
+  {anouncement: "make cards for teacher appreciation week", key: "7"},
+]) 
