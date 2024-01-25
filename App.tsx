b41b3d60fdc80d507f73883931cd40fdd0f7b916
BaqirSalim <101154594+BaqirSalim@users.noreply.github.com>
2024-01-25 11:54:06 -0600

    fixed position of dummy array

--- App.tsx
+++ App.tsx
@@ -2,6 +2,19 @@ import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [dummyAnnouncements, setDummyAnnouncements] = useState([
+    { announcement: "free food on Wednesday", key: "1" },
+    { announcement: "wear festive clothes for 13th Rajab", key: "2" },
+    { announcement: "eat more food to prevent cancer", key: "3" },
+    { announcement: "sign up for SAT prep", key: "4" },
+    {
+      announcement: "Protest for Palestine in downtown houston on Jan 26th",
+      key: "5",
+    },
+    { announcement: "sign up for highschool fieldtrip on Feb 16th", key: "6" },
+    { announcement: "make cards for teacher appreciation week", key: "7" },
+  ]);
+
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 100 }}>This is the Al-Hadi App</Text>
@@ -12,21 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "blue",
     alignItems: "center",
     justifyContent: "center",
   },
 });
-
-const [dummyAnnouncements, setDummyAnnouncements] = useState([
-  { announcement: "free food on Wednesday", key: "1" },
-  { announcement: "wear festive clothes for 13th Rajab", key: "2" },
-  { announcement: "eat more food to prevent cancer", key: "3" },
-  { announcement: "sign up for SAT prep", key: "4" },
-  {
-    announcement: "Protest for Palestine in downtown houston on Jan 26th",
-    key: "5",
-  },
-  { announcement: "sign up for highschool fieldtrip on Feb 16th", key: "6" },
-  { announcement: "make cards for teacher appreciation week", key: "7" },
-]);
